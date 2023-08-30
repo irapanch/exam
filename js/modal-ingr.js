a503d8b58e828d9b92e822fed543dcad2ac5a4ec
irapanch <117355580+irapanch@users.noreply.github.com>
2023-08-30 14:24:38 +0300

    v2

--- js/modal-ingr.js
+++ js/modal-ingr.js
@@ -1,18 +1,18 @@
 
-import axios from 'axios'; 
-const divs = document.querySelector('.modal2');
-console.log(divs);
+// import axios from 'axios'; 
+// const divs = document.querySelector('.modal2');
+// console.log(divs);
 
-let dataIngredient = [];
-const BASE_URL = 'https://drinkify-backend.p.goit.global/api/v1/cocktails/';
-export async function getElement(name) {
+// let dataIngredient = [];
+// const BASE_URL = 'https://drinkify-backend.p.goit.global/api/v1/cocktails/';
+// export async function getElement(name) {
 
 
-  try {
-    const response = await axios.get(`${BASE_URL}?r=${name}`); // об'єкт, що містить результати запиту
-    return response.data; // виводимо результати запиту
-  } catch (error) {
-    console.log('error', error);
-  }
-}
-getElement()
+//   try {
+//     const response = await axios.get(`${BASE_URL}?r=${name}`); // об'єкт, що містить результати запиту
+//     return response.data; // виводимо результати запиту
+//   } catch (error) {
+//     console.log('error', error);
+//   }
+// }
+// getElement()
